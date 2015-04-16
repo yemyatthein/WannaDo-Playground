c77fb6bf7a635b8b55e5f5c44c0b2f60823b24ec
YeMyat Thein <yemyatthein@gmail.com>
2015-04-17 00:15:43 +0100

    remove typo

--- app/src/androidTest/java/com/yemyatthein/wannado/FullTestSuite.java
+++ app/src/androidTest/java/com/yemyatthein/wannado/FullTestSuite.java
@@ -4,8 +4,7 @@ import android.test.suitebuilder.TestSuiteBuilder;
 import junit.framework.Test;
 import junit.framework.TestSuite;
 
-public class exit
-        FullTestSuite extends TestSuite {
+public class FullTestSuite extends TestSuite {
     public static Test suite() {
         return new TestSuiteBuilder(FullTestSuite.class)
                 .includeAllPackagesUnderHere().build();
